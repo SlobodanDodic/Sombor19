d6e7509bc7b8dce6b7288fca2cbb6d2a9302933b
SlobodanDodic <slobodandodic@gmail.com>
2023-10-07 21:36:21 +0200

    testimonial again

--- app/[lang]/components/home/Testimonial.tsx
+++ app/[lang]/components/home/Testimonial.tsx
@@ -8,10 +8,10 @@ export function Testimonial() {
         <figure className="relative overflow-hidden mr-2 w-64">
           <span className="absolute top-3 left-3 text-5xl text-amber-600 z-20">“</span>
           <span className="absolute bottom-20 right-3 text-5xl text-amber-600 z-20">„</span>
-          <p className="relative -mb-10 rounded-lg bg-stone-800 text-sm text-white pt-9 pb-12 px-5">
-            The apartment was spacious and super clean with everything you'd need. It is located close to downtown and I
-            parked my vehicle and walked to buy groceries and get to a bank. I received help with my bags both arriving
-            and leaving as well as...
+          <p className="relative -mb-10 rounded-lg bg-stone-800 text-sm text-white pt-9 pb-14 px-5">
+            The apartment was spacious and super clean with everything you&apos;d need. It is located close to downtown
+            and I parked my vehicle and walked to buy groceries and get to a bank. I received help with my bags both
+            arriving and leaving as well as...
           </p>
           <div className="relative flex justify-end items-center h-20">
             <Image
