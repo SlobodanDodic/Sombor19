8b97accb835196170798ce2d65e5090701ee56a6
SlobodanDodic <slobodandodic@gmail.com>
2023-10-12 14:53:14 +0200

    gallery

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import me from "../assets/about/me.png";
-import brush1 from "../assets/about/brush1.png";
-import brush2 from "../assets/about/brush2.png";
-import meKer from "../assets/about/meKer.jpg";
-import sombor from "../assets/about/sombor.png";
+import me1 from "../assets/about/me-1.png";
+import me2 from "../assets/about/me-2.jpg";
+import brush1 from "../assets/about/brush-1.png";
+import brush2 from "../assets/about/brush-2.png";
+import Gallery from "../components/about/Gallery";
 
 export default function About() {
   return (
@@ -20,7 +20,7 @@ export default function About() {
             />
           </svg>
           <Image
-            src={me}
+            src={me1}
             alt="me"
             width={200}
             height={200}
@@ -30,7 +30,7 @@ export default function About() {
         </div>
 
         <div className="relative flex w-full min-h-[192px] justify-center items-center md:items-end md:mt-4">
-          <p className="text-sm md:text-base text-right md:text-left px-12 py-6 md:py-0 text-shadow-drop shadow-black/40">
+          <p className="text-sm md:text-base text-right md:text-left pl-8 pr-12 py-6 md:py-0 font-bold text-stone-600">
             Greetings! I&apos;m your friendly host in Sombor, passionate about welcoming travelers to my beautiful
             apartments. Traveling is my ultimate joy, allowing me to explore diverse cultures and make lifelong friends.
             I cherish the friendship of my extensive social circle and love connecting with people from all walks of
@@ -57,14 +57,14 @@ export default function About() {
         />
         <div className="relative">
           <Image
-            src={meKer}
-            alt="meKer"
+            src={me2}
+            alt="me2"
             width={500}
             height={500}
             className="w-52 h-52 rounded-full float-right mx-4 drop-shadow-sm"
             style={{ shapeOutside: "circle(50%)" }}
           />
-          <p className="text-justify md:text-right text-sm md:text-base text-shadow-drop shadow-black/40 px-8 md:pt-4">
+          <p className="text-justify md:text-right text-sm md:text-base font-bold text-stone-600 px-8 md:pt-4">
             In my apartments, you&apos;ll discover more than just a place to stay; you&apos;ll find a home away from
             home. I pour my love for travel and life into every detail, ensuring your stay is not just comfortable but
             also filled with delightful moments. Join me on this journey in Sombor, where you can experience the
@@ -82,6 +82,37 @@ export default function About() {
         style={{ objectFit: "contain" }}
         className="w-full my-20"
       /> */}
+
+      <h1 className="mt-16">My hometown</h1>
+      <h1 className="">Sombor</h1>
+
+      <div className="relative flex flex-col w-full max-w-3xl justify-center items-center mt-5">
+        <p className="text-sm md:text-base font-bold text-stone-600 px-8 py-4">
+          Nestled within the heart of Serbia, my hometown Sombor combines the charm of a cozy café with the vibrancy of
+          an artistic haven. After a day of dedicated work, I seek solace in the rhythmic pedal strokes along our
+          picturesque canal, savor invigorating swims in the Danube, and relish the aroma of freshly brewed coffee
+          drifting from our myriad cafes. As evening descends, Sombor transforms into a culinary paradise where I
+          indulge in elegant dining at fine restaurants. It's a sanctuary of tranquility, inviting weary souls to unwind
+          amidst the gentle ebb of time, a stark contrast to the hurried pace of urban life.
+        </p>
+
+        <Gallery />
+
+        <p className="text-sm md:text-base font-bold text-stone-600 px-8 py-8">
+          However, tranquility here does not mean monotony. Sombor teems with opportunities for adventure. Embark on
+          horseback journeys, explore waterways by boat, pedal through scenic trails, dance to your heart's content, or
+          take a plunge into the refreshing waters. Our cultural tapestry is woven with diverse threads, from gripping
+          theatrical performances to the thrill of fishing. Sombor isn't just a location; it's a vibrant canvas awaiting
+          the strokes of exploration, inviting travelers to immerse themselves in a plethora of enriching experiences.
+        </p>
+        <p className="text-sm md:text-base font-bold text-stone-600 px-8 pb-4">
+          Adding to its allure, Sombor (Serbian Cyrillic: Сомбор) stands as a city and the administrative center of the
+          West Bačka District in the autonomous province of Vojvodina, Serbia. The city boasts a population of 41,814,
+          while its administrative area, encompassing neighboring villages, shelters 71,521 inhabitants. This rich
+          cultural tapestry, combined with the city's tranquil charm, creates a unique haven for travelers seeking both
+          serenity and excitement.
+        </p>
+      </div>
     </div>
   );
 }
